eefdf2f9e124d17ca37c97705e13e4fb24c3536e
Mario Petrov <maripetrov33@gmail.com>
2023-10-12 18:36:07 +0300

    Edited regex and components placment

--- src/com/code/TitleConvert.java
+++ src/com/code/TitleConvert.java
@@ -15,20 +15,28 @@ public class TitleConvert {
     private  JButton clearButton;
     private JLabel outputLabel;
 
-
     public TitleConvert() {
         // Create main frame
         frame = new JFrame("Title Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(null);
 
-        // Create components
+        // Create and place components
         inputField = new JTextField(20);
+        inputField.setBounds(20,25,250,30);
+
         editButton = new JButton("Edit");
+        editButton.setBounds(300,25,70,30);
+
         copyButton = new JButton("Copy");
+        copyButton.setBounds(300,65,70,30);
+
         clearButton = new JButton("Clear");
+        clearButton.setBounds(300,105,70,30);
+
         outputLabel = new JLabel("Edited Text Will Appear Here");
+        outputLabel.setBounds(20,75,280,30);
 
         // Add components to the frame
         frame.add(inputField);
@@ -41,21 +49,20 @@ public class TitleConvert {
         editButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String inputText = inputField.getText();
-                String[] words = inputText.split("[. \t]+");
+                String[] words = inputText.split("[\\p{Punct}\\s]+");
                 StringBuilder finalText = new StringBuilder();
 
                 //Check if input starts with number
                 //If it does - convert to _NUM_
                 //If not - just start with capital letter
                 if(isNumber(words[0])){
-                finalText.append("_")
-                        .append(words[0])
-                        .append("_");
+                    finalText.append("_")
+                            .append(words[0])
+                            .append("_");
                     for (int i = 1; i < words.length; i++) {
-                         finalText.append(convertToTitleCase(words[i]));
+                        finalText.append(convertToTitleCase(words[i]));
                     }
                 }
-
                 else{
                     for (int i = 0; i < words.length; i++) {
                         finalText.append(convertToTitleCase(words[i]));
@@ -68,14 +75,12 @@ public class TitleConvert {
         // Add action listener to the copy button
         copyButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
                 String editedText = outputLabel.getText().substring("Edited Text: ".length());
 
                 // Copy to the clipboard
                 StringSelection stringSelection = new StringSelection(editedText);
                 Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                 clipboard.setContents(stringSelection, null);
-
             }
         });
 
@@ -88,19 +93,15 @@ public class TitleConvert {
             }
         });
 
-
         frame.setVisible(true);
     }
-
-
-
-
-
 
     // Convert a word to title case
     private static String convertToTitleCase(String word) {
         return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
     }
+
+    // Check if String is a number
     public static boolean isNumber(String str) {
         try {
             Integer.parseInt(str);
@@ -117,5 +118,4 @@ public class TitleConvert {
             }
         });
     }
-
 }
